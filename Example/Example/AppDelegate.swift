d5847eb4ff1f1a9fa735dc1a36d072fab006cd3f
ShunsukeFurubayashi <f.s.1992.ip@gmail.com>
2016-07-11 09:50:41 +0900

    Add empty projects

---
+++ Example/Example/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by 古林俊佑 on 2016/07/11.
+//  Copyright © 2016年 Shunsuke Furubayashi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
