ca13697f111b83b11acfc66a5b7940b7dc10740b
Econa77 <s.f.1992.ip@gmail.com>
2020-03-12 13:41:45 +0900

    Support SwiftPackageManager

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "Screeen",
+    platforms: [
+      .macOS(.v10_10)
+    ],
+    products: [
+        .library(
+            name: "Screeen",
+            targets: ["Screeen"]),
+    ],
+    targets: [
+        .target(
+            name: "Screeen",
+            dependencies: [],
+            path: "Lib/Screeen"),
+        .testTarget(
+            name: "ScreeenTests",
+            dependencies: ["Screeen"],
+            path: "Lib/ScreeenTests"),
+    ],
+    swiftLanguageVersions: [.v5]
+)
