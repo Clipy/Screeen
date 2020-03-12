69d32eddd88458cd6c9a456cb3930e435401776e
Econa77 <s.f.1992.ip@gmail.com>
2020-03-12 13:34:17 +0900

    Support macOS 10.15 Catalina security features (#13)
    * Support macOS 10.15 Catalina security features
    
    * Not set when scopes is empty
    
    * Drop support macOS 10.9
    
    * Fix deployment target version
    
    * Separate start query method
    
    * Update README.md
    
    * Update README.md

--- Lib/Screeen/ScreenShotObserver.swift
+++ Lib/Screeen/ScreenShotObserver.swift
@@ -28,30 +28,53 @@ public final class ScreenShotObserver: NSObject {
     public weak var delegate: ScreenShotObserverDelegate?
     public var isEnabled = true
 
+    private static let desktopDirectoryPath: String? = NSSearchPathForDirectoriesInDomains(.desktopDirectory, .userDomainMask, true).first
     private let query = NSMetadataQuery()
+    private let notificationCenter: NotificationCenter
 
     // MARK: - Initialize
-    override public init() {
+    public override convenience init() {
+        self.init(searchDirectoryPaths: [ScreenShotObserver.desktopDirectoryPath].compactMap { $0 }, notificationCenter: .default)
+    }
+
+    public convenience init(searchDirectoryPaths: [String]) {
+        self.init(searchDirectoryPaths: searchDirectoryPaths, notificationCenter: .default)
+    }
+
+    init(searchDirectoryPaths: [String], notificationCenter: NotificationCenter) {
+        self.notificationCenter = notificationCenter
         super.init()
         // Observe update notification
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(ScreenShotObserver.updateQuery(_:)),
-                                               name: NSNotification.Name.NSMetadataQueryDidUpdate,
-                                               object: query)
+        notificationCenter.addObserver(self,
+                                       selector: #selector(ScreenShotObserver.updateQuery(_:)),
+                                       name: NSNotification.Name.NSMetadataQueryDidUpdate,
+                                       object: query)
         // Query setting
         query.delegate = self
+        if !searchDirectoryPaths.isEmpty {
+            query.searchScopes = searchDirectoryPaths
+        }
         query.predicate = NSPredicate(format: "kMDItemIsScreenCapture = 1")
-        query.start()
     }
 
     deinit {
-        NotificationCenter.default.removeObserver(self)
+        notificationCenter.removeObserver(self)
         query.stop()
         query.delegate = nil
     }
 
+    // MARK: - Start
+    public func start() {
+        query.start()
+    }
+
+    public func stop() {
+        query.stop()
+    }
+
+    // MARK: - Notification
     @objc func updateQuery(_ notification: Notification) {
-        if !isEnabled { return }
+        guard isEnabled else { return }
 
         if let items = notification.userInfo?[kMDQueryUpdateAddedItems as String] as? [NSMetadataItem] {
             items.forEach { delegate?.screenShotObserver(self, addedItem: $0) }
--- README.md
+++ README.md
@@ -22,6 +22,7 @@ github "Clipy/Screeen"
 ```swift
 let observer = ScreenShotObserver()
 observer.delegate = self
+observer.start()
 ```
 
 ```swift
@@ -42,6 +43,34 @@ Change observing status
 ```swift
 observer.isEnabled = false // Stop observing
 observer.isEnabled = true  // Restart observing
+```
+
+## Security features of macOS 10.15 or higher
+Security features have been enhanced since mac10.15 Catalina, and screenshot monitoring must be granted permission. The monitoring target can be specified for each directory, and the desktop directory is specified by default.　
+
+When call `start()` for the first time, a permission alert will be displayed and monitoring will start when allow it.
+
+### Desktop directory monitoring (default)
+```
+let observer = ScreenShotObserver()
+```
+
+### Desktop and Picture directories monitoring
+```
+let desktopPath = NSSearchPathForDirectoriesInDomains(.desktopDirectory, .userDomainMask, true).first
+let picturePath = NSSearchPathForDirectoriesInDomains(.pictureDirectory, .userDomainMask, true).first
+let directoryPaths = [desktopPath, picturePath].compactMap { $0 }
+let observer = ScreenShotObserver(searchDirectoryPaths: directoryPaths)
+```
+
+### Monitoring all screenshots on macOS 10.14 or earlier
+```
+let observer: ScreenShotObserver
+if #available(macOS 10.15, *) {
+    observer = ScreenShotObserver()
+} else {
+    observer = ScreenShotObserver(searchDirectoryPaths: [])
+}
 ```
 
 ### Contributing
